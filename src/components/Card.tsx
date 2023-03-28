81ca824d1621b2c1e86e609aef09daa9e7f7257c
bengin34 <becaglar3434@gmail.com>
2023-03-28 20:55:04 +0300

    changes

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import { BsPlus } from "react-icons/bs";
+import postData from "../helpers/fetchData";
+
+
 const Card = () => {
   const [image, setImage] = useState("");
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
 
+  console.log(title)
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     console.log(file);
@@ -20,13 +24,19 @@ const Card = () => {
   };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    console.log("calisiyor")
     e.preventDefault();
-
+     postData(title, description, image);
+      setTitle("");
+      setDescription("");
+      setImage("");
+    
   };
+  
 
   return (
     <>
-      <div className="flex gap-8 items-center justify-center min-h-screen from-[#F9F5F3] via-[#F9F5F3] to-[#F9F5F3] bg-gradient-to-br px-2">
+      <div className="flex flex-col gap-8 items-center justify-center min-h-screen from-[#F9F5F3] via-[#F9F5F3] to-[#F9F5F3] bg-gradient-to-br px-2">
         <form onSubmit={handleSubmit}>
           <p className="text-black border w-24 rounded-t-lg py-1 px-2 bg-white">
             New Title
@@ -37,11 +47,12 @@ const Card = () => {
                 <input
                   placeholder="New Title"
                   className="block  px-2 py-1 outline-none"
-                //   onChange={() => }
+                    onChange={(e) => setTitle(e.target.value)}
                 />
                 <textarea
                   placeholder="New description"
                   className="font-bold border w-full rounded-lg text-gray-700 text-sm px-2 py-2 leading-7 mb-1 outline-none"
+                  onChange={(e) => setDescription(e.target.value)}
                 />
 
                 {image ? (
@@ -85,17 +96,24 @@ const Card = () => {
           </div>
         </form>
 
-        <div className="h-[446px]">
-          <p className="text-black border w-24  rounded-t-lg py-1 px-2 bg-white">
+        <div className="">
+          <p className="text-black border w-24 rounded-t-lg py-1 px-2 bg-white">
             New Title
           </p>
-          <div className="w-80  mx-auto bg-white h-72  shadow overflow-hidden">
-            <div className="max-w-md   mx-auto">
-              <div className="p-4 sm:p-6 ">
-                <p>sadsad</p>
-                <p className="h-24">sadsad</p>
+          <div className="w-80  mx-auto bg-white  shadow overflow-hidden">
+            <div className="max-w-md mx-auto">
+              <div className="p-4 sm:p-6">
+                <h3 className="block  px-2 py-1 h-16 outline-none"></h3>
+                <p
+                  
+                  className="font-bold  w-full h-24 rounded-lg text-gray-700 text-sm px-2 py-2 leading-7 mb-1 outline-none"
+                >
+                  {" "}
+                </p>
 
-                <div className="flex items-center  h-52 bg-gray-300 justify-center w-full"></div>
+                <div className="flex items-center justify-center w-full ">
+                  <div className="flex flex-col items-center  justify-center w-full h-52 border-2 py-2 border-[#1B2A39] rounded-lg  bg-gray-500 hover:opacity-80"></div>
+                </div>
               </div>
             </div>
           </div>
