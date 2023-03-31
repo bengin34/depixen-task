51dc8dd11d76ef8b6aa14ce060b9ada99cadf961
bengin34 <becaglar3434@gmail.com>
2023-03-31 15:20:44 +0300

    test

--- src/helpers/fetchData.tsx
+++ src/helpers/fetchData.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 import { collection, addDoc,getDocs, setDoc, doc } from "firebase/firestore";
 
-import { database } from "../firebaseConfig";
+import { database } from "./firebaseConfig";
 
 const BASE_URL = "http://0.0.0.0:8000/users";
 
@@ -34,40 +34,3 @@ export const getImage = async () => {
 }
 
 
-
-// import axios from "axios";
-// import { collection, addDoc, getDocs, query, orderBy } from "firebase/firestore";
-// import { database } from "../firebaseConfig";
-
-// const BASE_URL = "http://0.0.0.0:8000/users";
-
-// interface Response {
-//   title: string;
-//   description: string;
-//   image: string;
-// }
-
-// export const postData = async (info:Response) => {
-//   const { data } = await axios.post(BASE_URL, info);
-//   await addDoc(collection(database, "images"), { imageUrl: info.image });
-//   return data;
-// };
-
-// export const getData = async () => {
-//   const { data } = await axios.get(BASE_URL);
-//   console.log(data);
-//   return data;
-// };
-
-// export const getImage = async () => {
-//   const imageQuery = query(collection(database, "images"), orderBy("createdAt", "desc"));
-//   const querySnapshot = await getDocs(imageQuery);
-//   if (querySnapshot.docs.length > 0) {
-//     const lastImage = querySnapshot.docs[0].data().imageUrl;
-//     console.log(lastImage);
-//     return lastImage;
-//   } else {
-//     console.log("No images found");
-//     return null;
-//   }
-// };
